82da9479179caf4002c86396b697a6fe9b891601
kathanshah1206 <97287116+kathanshah1206@users.noreply.github.com>
2023-12-14 11:13:40 +0530

    Update testing.js

--- backend/testing.js
+++ backend/testing.js
@@ -1,31 +1,32 @@
-const bodyParser = require('body-parser');
-const express = require('express');
-var cors = require('cors')
+const bodyParser = require("body-parser");
+const express = require("express");
+var cors = require("cors");
 
 const app = express();
-const port = 8080; 
-app.use(cors())
+const port = 5500;
+app.use(cors());
 
 app.use(bodyParser.json());
 
-
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send('{"name":"John", "age":30, "car":null} ');
 });
 
-app.post('/appo', (req, res) => {
- let k=req.body;
+app.post("/appo", (req, res) => {
+  let k = req.body;
 
- console.log(k);
- res.send('backend message ');
+  console.log(k);
+
+  res.send("backend message sent ");
 });
 
-
-
-app.get('/appo', (req, res) => {
-    res.send('{"kathan":"shah"}');
+app.get("/appo", (req, res) => {
+  let param = {
+    age: 56,
+    name: "pqr",
+  };
+  res.send(param);
 });
-
 
 app.listen(port, () => {
   console.log(`Server is running `);
